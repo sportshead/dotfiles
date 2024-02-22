db476e62486d7cd4c3635690d7bfd36581b5cab8
sportshead <me@sportshead.dev>
2024-02-22 19:38:10 +0800

    update proxy alias, bump nvim

--- .bash.d/aliases.sh
+++ .bash.d/aliases.sh
@@ -36,5 +36,5 @@ if [[ "$OSTYPE" =~ ^darwin ]]; then
 
     alias signal="nohup /Applications/Signal.app/Contents/MacOS/Signal </dev/null >/dev/null 2>&1 & disown"
 
-    alias proxy="export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087;export ALL_PROXY=socks5://127.0.0.1:1080"
+    alias proxy="export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087;export ALL_PROXY=socks5://127.0.0.1:1080;export NO_PROXY=\"localhost,127.0.0.1\""
 fi
